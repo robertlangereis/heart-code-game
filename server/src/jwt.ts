2e3a53a3fc559f63437a99c4d699dfaba5b8cd65
robertlangereis <robert.langereis@gmail.com>
2019-06-04 10:06:46 +0200

    improve indentation, cleanup of redundant code and console.logs

--- server/src/jwt.ts
+++ server/src/jwt.ts
@@ -1,14 +1,14 @@
-import * as jwt from 'jsonwebtoken'
+import * as jwt from "jsonwebtoken";
 
-export const secret = process.env.JWT_SECRET || '9u8nnjksfdt98*(&*%T$#hsfjk'
-const ttl = 3600 * 4 // our JWT tokens are valid for 4 hours
+export const secret = process.env.JWT_SECRET || "9u8nnjksfdt98*(&*%T$#hsfjk";
+const ttl = 3600 * 4; // our JWT tokens are valid for 4 hours
 
 interface JwtPayload {
-  id: number
+  id: number;
 }
 
 export const sign = (data: JwtPayload) =>
-  jwt.sign(data, secret, { expiresIn: ttl })
+  jwt.sign(data, secret, { expiresIn: ttl });
 
 export const verify = (token: string): JwtPayload =>
-  jwt.verify(token, secret) as JwtPayload
+  jwt.verify(token, secret) as JwtPayload;
--- server/src/users/controller.ts
+++ server/src/users/controller.ts
@@ -1,39 +1,41 @@
-import { JsonController, Post, Param, Get, Body, Authorized } from 'routing-controllers'
-import User from './entity';
-import { io } from '../index'
+import {
+  JsonController,
+  Post,
+  Param,
+  Get,
+  Body,
+  Authorized,
+} from "routing-controllers";
+import User from "./entity";
+import { io } from "../index";
 
 @JsonController()
 export default class UserController {
+  @Post("/users")
+  async signup(@Body() data: User) {
+    const { password, ...rest } = data;
+    const entity = User.create(rest);
+    await entity.setPassword(password);
 
-  @Post('/users')
-  async signup(
-    @Body() data: User
-  ) {
-    const {password, ...rest} = data
-    const entity = User.create(rest)
-    await entity.setPassword(password)
+    const user = await entity.save();
 
-    const user = await entity.save()
+    io.emit("action", {
+      type: "ADD_USER",
+      payload: entity,
+    });
 
-    io.emit('action', {
-      type: 'ADD_USER',
-      payload: entity
-    })
-
-    return user
+    return user;
   }
 
   @Authorized()
-  @Get('/users/:id([0-9]+)')
-  getUser(
-    @Param('id') id: number
-  ) {
-    return User.findOneById(id)
+  @Get("/users/:id([0-9]+)")
+  getUser(@Param("id") id: number) {
+    return User.findOneById(id);
   }
 
   @Authorized()
-  @Get('/users')
+  @Get("/users")
   allUsers() {
-    return User.find()
+    return User.find();
   }
 }
